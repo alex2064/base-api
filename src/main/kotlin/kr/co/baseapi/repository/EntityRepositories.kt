8feb8b52e5795416a0c07b9c4e8ca446630533f4
alex2064 <kimkimgodd@naver.com>
2024-02-26 22:10:44 +0900

    bulk 연산 추가
    바로 DB에 update 구문이 날아감

--- src/main/kotlin/kr/co/baseapi/repository/EntityRepositories.kt
+++ src/main/kotlin/kr/co/baseapi/repository/EntityRepositories.kt
@@ -5,8 +5,15 @@ import kr.co.baseapi.entity.Family
 import kr.co.baseapi.entity.Member
 import kr.co.baseapi.entity.Parent
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
 
-interface MemberRepository : JpaRepository<Member, Long>
+interface MemberRepository : JpaRepository<Member, Long> {
+
+    @Modifying(clearAutomatically = true)
+    @Query("update Member set isAuth = :isAuth where memberId in (:ids)")
+    fun saveIsAuth(isAuth: Boolean, ids: List<Long>)
+}
 
 interface FamilyRepository : JpaRepository<Family, Long>
 
