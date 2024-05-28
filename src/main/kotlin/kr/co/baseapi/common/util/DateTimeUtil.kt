eba651e0d6c872056ff8f985296c1711b48a27fa
alex2064 <kimkimgodd@naver.com>
2024-05-28 21:20:51 +0900

    현재일자 ~ 끝일자까지의 LocalDate List 뽑는 중위함수 추가

--- src/main/kotlin/kr/co/baseapi/common/util/DateTimeUtil.kt
+++ src/main/kotlin/kr/co/baseapi/common/util/DateTimeUtil.kt
@@ -23,3 +23,20 @@ fun LocalDate.toString8(): String = this.format(BASIC_ISO_DATE)
  * LocalDate -> String(yyyy-MM-dd)
  */
 fun LocalDate.toString10(): String = this.format(ISO_LOCAL_DATE)
+
+/**
+ * 현재일자 ~ toDate 까지 LocalDate List
+ */
+infix fun LocalDate.toDatesUntil(toDate: LocalDate): List<LocalDate> =
+    if (toDate.isBefore(this)) {
+        listOf()
+    } else {
+        datesUntilSequence(this, toDate).toList()
+    }
+
+private fun datesUntilSequence(frDate: LocalDate, toDate: LocalDate): Sequence<LocalDate> =
+    generateSequence(frDate) { it.plusDays(1L) }
+        .takeWhile { it.isBefore(toDate) || it == toDate }  // it <= toDate ??
+
+
+
